d40efedec4b34736615026361fddab78c0deed30
lemonschen <chenzhiyuml@foxmail.com>
2020-11-15 01:16:55 +0800

    init

---
+++ springboot-study-ftp/src/main/java/uninut/top/ftp/SpringbootStudyFtpApplication.java
@@ -0,0 +1,13 @@
+package uninut.top.ftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyFtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyFtpApplication.class, args);
+    }
+
+}
---
+++ springboot-study-ftp/src/test/java/uninut/top/ftp/SpringbootStudyFtpApplicationTests.java
@@ -0,0 +1,13 @@
+package uninut.top.ftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyFtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
