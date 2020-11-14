98c9ec4f2d737fc4d5fa58d915aae3e8732e7f6c
lemonschen <chenzhiyuml@foxmail.com>
2020-11-15 00:53:20 +0800

    init

---
+++ springboot-study-core/src/main/java/top/uninut/core/SpringbootStudyCoreApplication.java
@@ -0,0 +1,13 @@
+package top.uninut.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyCoreApplication.class, args);
+    }
+
+}
---
+++ springboot-study-core/src/test/java/top/uninut/core/SpringbootStudyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package top.uninut.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
